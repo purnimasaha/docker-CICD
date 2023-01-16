3ab19a2a17045646f956bb6ebbad3b67dfb6a9fd
Saha <930148@cognizant.com>
2023-01-16 16:06:22 +0530

    first commit

---
+++ src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
@@ -0,0 +1,35 @@
+package com.javatechie.aws.cicd.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.core.recovery.ResilientSyslogOutputStream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/orders")
+public class OrderServiceApplication {
+
+    @Autowired
+    private OrderDao orderDao;
+
+    @GetMapping
+    public List<Order> fetchOrders() {
+    	System.out.println("start");
+        return orderDao.getOrders().stream().
+                sorted(Comparator.comparing(Order::getPrice)).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+}
